d5655bb4d4ac382fd8179e14626fb076dd84cc72
fwh-shuai <545963582@qq.com>
2020-01-03 11:50:32 +0800

    引入tab试试

---
+++ src/引入按钮.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import { Button } from 'antd-mobile';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button>我是ant-design的按鈕啊</Button>
+      </div>
+    )
+  }
+}
